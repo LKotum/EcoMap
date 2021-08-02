93d781a1c12fd31aaa1404625613a05e51c277a9
NeverLux <NeverLuxRus@gmail.com>
2021-08-02 15:47:41 +0300

    Move

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float forceMove;
+    public float forceRotate;
+    public Vector3 move;
+    public Transform mainCamera;
+
+
+    void Start()
+    {
+        mainCamera = this.gameObject.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        move = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        mainCamera.Translate(move * Time.deltaTime * forceMove, Space.World);
+        if(Input.GetKey(KeyCode.E)){
+            mainCamera.Rotate(.0f, 1.0f * forceRotate, .0f, Space.World);
+        }
+
+        if(Input.GetKey(KeyCode.Q)){
+            mainCamera.Rotate(.0f, -1.0f * forceRotate, .0f, Space.World);
+        }
+        
+    }
+}
