8d6e4d5cb3664d1d4aa23ba0feb9d57db7fe1790
NeverLux <NeverLuxRus@gmail.com>
2021-08-02 16:23:54 +0300

    Move_1

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,24 +7,26 @@ public class Controller : MonoBehaviour
     public float forceMove;
     public float forceRotate;
     public Vector3 move;
+    public Vector3 moveCamera;
     public Transform mainCamera;
 
 
     void Start()
     {
-        mainCamera = this.gameObject.GetComponent<Transform>();
+        //mainCamera = this.gameObject.GetComponent<Transform>();
     }
 
     void Update()
     {
-        move = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
-        mainCamera.Translate(move * Time.deltaTime * forceMove, Space.World);
+        move = mainCamera.transform.forward;
+        moveCamera = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        this.gameObject.transform.Translate(Input.GetAxis("Vertical") * move * Time.deltaTime * forceMove, Space.Self);
         if(Input.GetKey(KeyCode.E)){
-            mainCamera.Rotate(.0f, 1.0f * forceRotate, .0f, Space.World);
+            mainCamera.transform.Rotate(.0f, 1.0f * forceRotate, .0f, Space.World);
         }
 
         if(Input.GetKey(KeyCode.Q)){
-            mainCamera.Rotate(.0f, -1.0f * forceRotate, .0f, Space.World);
+            mainCamera.transform.Rotate(.0f, -1.0f * forceRotate, .0f, Space.World);
         }
         
     }
