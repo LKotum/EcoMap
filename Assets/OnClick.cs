5c95aefa9674a7b40c8d748229d426eb40376da8
NeverLux <NeverLuxRus@gmail.com>
2021-08-03 23:11:13 +0300

    MoveCamera

---
+++ Assets/OnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour
+{
+    public GameObject cameraTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        cameraTransform.GetComponent<Controller>().destination = transform.position;
+        cameraTransform.GetComponent<Controller>().movePosition = true;
+    }
+}
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,33 +1,68 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class Controller : MonoBehaviour
 {
     public float forceMove;
     public float forceRotate;
-    public Vector3 move;
-    public Vector3 moveCamera;
     public Transform mainCamera;
+
+    public Text X,Y,Z;
+    public float speed = 0.01f;
+    public Vector3 destination;
+
+    public bool movePosition = false;
 
 
     void Start()
     {
-        //mainCamera = this.gameObject.GetComponent<Transform>();
+        
+        destination = transform.position;
+        mainCamera = this.gameObject.GetComponent<Transform>();
     }
 
     void Update()
     {
-        move = mainCamera.transform.forward;
-        moveCamera = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
-        this.gameObject.transform.Translate(Input.GetAxis("Vertical") * move * Time.deltaTime * forceMove, Space.Self);
-        if(Input.GetKey(KeyCode.E)){
-            mainCamera.transform.Rotate(.0f, 1.0f * forceRotate, .0f, Space.World);
-        }
+        if(movePosition){
+            transform.position = Vector3.Lerp(transform.position, destination, speed);
+            if(Vector3.Distance(transform.position, destination) < 5f) movePosition = false;
+        }else{
+            X.text = "X = " + Convert.ToString(Math.Round(mainCamera.position.x, 2));
+            Z.text = "Z = " + Convert.ToString(Math.Round(mainCamera.position.z, 2));
+            Y.text = Convert.ToString(Math.Round(mainCamera.position.y, 2));
 
-        if(Input.GetKey(KeyCode.Q)){
-            mainCamera.transform.Rotate(.0f, -1.0f * forceRotate, .0f, Space.World);
+            mainCamera.position += mainCamera.forward * forceMove * Input.GetAxis("Vertical");
+            mainCamera.position += mainCamera.right * forceMove * Input.GetAxis("Horizontal");
+            //this.gameObject.transform.Translate(Input.GetAxis("Vertical") * move * Time.deltaTime * forceMove, Space.Self);
+            if(Input.GetKey(KeyCode.E)){
+                mainCamera.transform.Rotate(.0f, 1.0f * forceRotate, .0f, Space.World);
+            }
+
+            if(Input.GetKey(KeyCode.Q)){
+                mainCamera.transform.Rotate(.0f, -1.0f * forceRotate, .0f, Space.World);
+            }
+
+            if(Input.GetKey(KeyCode.Z)){
+                mainCamera.position += mainCamera.up * forceMove;
+            }
+
+            if(Input.GetKey(KeyCode.X)){
+                mainCamera.position -= mainCamera.up * forceMove;
+            }
         }
         
+
+
+
+
+        
+        
     }
+
+
+
+    
 }
