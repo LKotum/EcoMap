69d5c3327babbb5c7036b5b7c65b83d3f473c1d9
Arseniy <88040466+LKotum@users.noreply.github.com>
2021-08-04 17:34:54 +0500

    Revert "Format"
    This reverts commit ea044dcbeb73cf1bd167ab67691e119793b5516b.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -6,57 +6,63 @@ using System;
 
 public class Controller : MonoBehaviour
 {
-    public float forceMove = 1;
-    public float forceRotate = 1;
-    //public Transform mainCamera;
+    public float forceMove;
+    public float forceRotate;
+    public Transform mainCamera;
 
-    public Text X, Y, Z;
+    public Text X,Y,Z;
     public float speed = 0.01f;
     public Vector3 destination;
+
     public bool movePosition = false;
 
 
     void Start()
     {
+        
         destination = transform.position;
+        mainCamera = this.gameObject.GetComponent<Transform>();
     }
 
     void Update()
     {
-        if (movePosition)
-        {
+        if(movePosition){
             transform.position = Vector3.Lerp(transform.position, destination, speed);
-            if (Vector3.Distance(transform.position, destination) < 5f) movePosition = false;
-        }
-        else
-        {
-            X.text = "X = " + Convert.ToString(Math.Round(this.gameObject.GetComponent<Transform>().position.x, 2));
-            Z.text = "Z = " + Convert.ToString(Math.Round(this.gameObject.GetComponent<Transform>().position.z, 2));
-            Y.text = Convert.ToString(Math.Round(this.gameObject.GetComponent<Transform>().position.y, 2));
+            if(Vector3.Distance(transform.position, destination) < 5f) movePosition = false;
+        }else{
+            X.text = "X = " + Convert.ToString(Math.Round(mainCamera.position.x, 2));
+            Z.text = "Z = " + Convert.ToString(Math.Round(mainCamera.position.z, 2));
+            Y.text = Convert.ToString(Math.Round(mainCamera.position.y, 2));
 
-            this.gameObject.GetComponent<Transform>().position += this.gameObject.GetComponent<Transform>().forward * forceMove * Input.GetAxis("Vertical");
-            this.gameObject.GetComponent<Transform>().position += this.gameObject.GetComponent<Transform>().right * forceMove * Input.GetAxis("Horizontal");
-
+            mainCamera.position += mainCamera.forward * forceMove * Input.GetAxis("Vertical");
+            mainCamera.position += mainCamera.right * forceMove * Input.GetAxis("Horizontal");
             //this.gameObject.transform.Translate(Input.GetAxis("Vertical") * move * Time.deltaTime * forceMove, Space.Self);
-            if (Input.GetKey(KeyCode.E))
-            {
-                this.gameObject.GetComponent<Transform>().transform.Rotate(.0f, 1.0f * forceRotate, .0f, Space.World);
+            if(Input.GetKey(KeyCode.E)){
+                mainCamera.transform.Rotate(.0f, 1.0f * forceRotate, .0f, Space.World);
             }
 
-            if (Input.GetKey(KeyCode.Q))
-            {
-                this.gameObject.GetComponent<Transform>().transform.Rotate(.0f, -1.0f * forceRotate, .0f, Space.World);
+            if(Input.GetKey(KeyCode.Q)){
+                mainCamera.transform.Rotate(.0f, -1.0f * forceRotate, .0f, Space.World);
             }
 
-            if (Input.GetKey(KeyCode.Z))
-            {
-                this.gameObject.GetComponent<Transform>().position += this.gameObject.GetComponent<Transform>().up * forceMove;
+            if(Input.GetKey(KeyCode.Z)){
+                mainCamera.position += mainCamera.up * forceMove;
             }
 
-            if (Input.GetKey(KeyCode.X))
-            {
-                this.gameObject.GetComponent<Transform>().position -= this.gameObject.GetComponent<Transform>().up * forceMove;
+            if(Input.GetKey(KeyCode.X)){
+                mainCamera.position -= mainCamera.up * forceMove;
             }
         }
+        
+
+
+
+
+        
+        
     }
+
+
+
+    
 }
