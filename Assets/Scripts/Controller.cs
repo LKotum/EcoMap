769895bcac102c542681f0f7e2d21db023c8f694
IvanShulga72 <shulga.ivan.dm@gmail.com>
2021-08-06 17:13:27 +0300

    сделал, чтобы подлетая к объекту вставал на место камеры (которая уже установлена у объекта) и плавное вращение при приближении

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -13,21 +13,23 @@ public class Controller : MonoBehaviour
     public Text X,Y,Z;
     public float speed = 0.01f;
     public Vector3 destination;
+    public Quaternion vra;
 
     public bool movePosition = false;
 
-
     void Start()
     {
-        
+        vra = transform.rotation;
         destination = transform.position;
         mainCamera = this.gameObject.GetComponent<Transform>();
     }
 
-    void Update()
-    {
+    void Update(){
+    
+       
         if(movePosition){
             transform.position = Vector3.Lerp(transform.position, destination, speed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, vra, speed);
             if(Vector3.Distance(transform.position, destination) < 5f) movePosition = false;
         }else{
             X.text = "X = " + Convert.ToString(Math.Round(mainCamera.position.x, 2));
@@ -36,7 +38,7 @@ public class Controller : MonoBehaviour
 
             mainCamera.position += mainCamera.forward * forceMove * Input.GetAxis("Vertical");
             mainCamera.position += mainCamera.right * forceMove * Input.GetAxis("Horizontal");
-            //this.gameObject.transform.Translate(Input.GetAxis("Vertical") * move * Time.deltaTime * forceMove, Space.Self);
+            
             if(Input.GetKey(KeyCode.E)){
                 mainCamera.transform.Rotate(.0f, 1.0f * forceRotate, .0f, Space.World);
             }
@@ -46,11 +48,11 @@ public class Controller : MonoBehaviour
             }
 
             if(Input.GetKey(KeyCode.Z)){
-                mainCamera.position += mainCamera.up * forceMove;
+                mainCamera.position += mainCamera.up * forceRotate;
             }
 
             if(Input.GetKey(KeyCode.X)){
-                mainCamera.position -= mainCamera.up * forceMove;
+                mainCamera.position -= mainCamera.up * forceRotate;
             }
         }
         
